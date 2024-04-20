62e50e099707971faa16689ea884dc046026b1ec
JulianaMistron <jumistron@yahoo.com.br>
2024-04-20 10:52:48 -0300

    Cria numero primo

--- numeroprimo/Program.cs
+++ numeroprimo/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, Wor");
+﻿int numero;
+int divisor = 0;
+
+Console.WriteLine("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= numero; i++)
+{
+    if (numero % i == 0)
+        divisor++;
+}
+
+if (divisor == 2)
+    Console.WriteLine("É um número primo.");
+
+else
+    Console.WriteLine("Não é um número primo.");
+
+
+Console.ReadKey();
